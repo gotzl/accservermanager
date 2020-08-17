774924ca5e45c7463dfe587cad7f8bd5e2dba87c
João Boing <joao@conectra.net.br>
2020-08-17 14:44:50 -0300

    Add STATIC_ROOT path to settings.py

--- accservermanager/settings.py
+++ accservermanager/settings.py
@@ -100,6 +100,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
 
 
